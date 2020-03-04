64feb4b4a81b4da583bbc40caf483cde91d99d35
Katherine Zaoral <kz@adhoc.com.ar>
2020-03-04 15:30:49 -0300

    [REM] l10n_ar_ux: remove funcitionality to prepare common use journals

--- l10n_ar_ux/models/account_chart_template.py
+++ l10n_ar_ux/models/account_chart_template.py
@@ -9,43 +9,11 @@ class AccountChartTemplate(models.Model):
 
     _inherit = 'account.chart.template'
 
-    # TODO we should move this to another module because not only argentina use this
-    # or use account_opening_move_id?
-    opening_clousure_account_id = fields.Many2one(
-        'account.account.template',
-        string='Opening / Closure Account',
-        domain=[('internal_type', '=', 'other'), ('deprecated', '=', False)],
-    )
-
     def _load(self, sale_tax_rate, purchase_tax_rate, company):
         """ Set non monetary tag when installing chart of account """
         res = super()._load(sale_tax_rate, purchase_tax_rate, company)
         self.env['account.account'].set_non_monetary_tag(company)
         return res
-
-    def _prepare_all_journals(self, acc_template_ref, company, journals_dict=None):
-        """ Add more journals commonly used in argentina localization """
-        journal_data = super()._prepare_all_journals(acc_template_ref, company, journals_dict)
-
-        opening_clousure_account_id = acc_template_ref.get(self.opening_clousure_account_id.id)
-        journals = [
-            # TODO change to english
-            (_('Liquidación de Impuestos'), 'LIMP', 'general', False),
-            (_('Sueldos y Jornales'), 'SYJ', 'general', False),
-            (_('Asientos de Apertura / Cierre'), 'A/C', 'general', opening_clousure_account_id),
-        ]
-        for name, code, journal_type, default_account_id in journals:
-            journal_data.append({
-                'type': journal_type,
-                'name': name,
-                'code': code,
-                'default_credit_account_id': default_account_id,
-                'default_debit_account_id': default_account_id,
-                'company_id': company.id,
-                'show_on_dashboard': False,
-                'update_posted': True,
-            })
-        return journal_data
 
     def _create_bank_journals(self, company, acc_template_ref):
         """ We create the withholding journal if withholding module installed """
