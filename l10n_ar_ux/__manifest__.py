33ba050812e7fa5bf32f679915556774a97587d5
Katherine Zaoral <kz@adhoc.com.ar>
2020-03-04 15:11:08 -0300

    [IMP] l10n_ar_ux: move functionalities from l10n_ar_account
    Update l10n_ar_ux with wath we have in l10n_ar_account and adapt it to
    13.0 version an l10n_ar official module:
    
    * hooks, move model and view, partner and company views, portal templates
    * remove bank_cbu, already integrated using acc_number in bank account
    * change references to field gross_income_type with l10n_ar_gross_income_type
    * move here functionality to show preview and final currency rate, adapt to work with odoo
    * make non monetary tag work, call it when loading chart of account
    * comment tax/type/application in withholding taxes until new model change
    * fix some misspelling errors
    * add missing api.depends,
    * remove api.multi
    * fix reference to old date_invoice field to invoice_date
    * update readme and module description
    * fix lints
    * add some todos
    * fix missing field in api.onchange
    * adapt view to make required fields in company form view.
    * add dependency to base_ux in order to automatically order provinces by name in company form view

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,21 +1,28 @@
 {
-    'name': 'Argentinian Localization UX',
+    'name': 'Argentinian Accounting UX',
     'version': "13.0.1.0.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
-    'author': 'ADHOC SA,Odoo Community Association (OCA)',
+    'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
     'summary': '',
     'depends': [
-        'l10n_ar',
+        'l10n_ar', 'base_ux',
     ],
     'data': [
         'data/res_currency_data.xml',
+        'data/account_account_tag_data.xml',
+        'data/account_tax_group_data.xml',
+        'views/portal_templates.xml',
+        'views/account_move_view.xml',
+        'views/res_company_view.xml',
+        'views/res_partner_view.xml',
     ],
     'demo': [
     ],
     'installable': True,
     'auto_install': True,
-    'application': True,
+    'application': False,
+    'post_init_hook': 'post_init_hook',
 }
--- l10n_ar_ux/models/res_company.py
+++ l10n_ar_ux/models/res_company.py
@@ -2,6 +2,14 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import controllers
-from . import models
-from .hooks import post_init_hook
+from odoo import fields, models, api
+
+
+class ResCompany(models.Model):
+
+    _inherit = "res.company"
+
+    gross_income_jurisdiction_ids = fields.Many2many(
+        related='partner_id.gross_income_jurisdiction_ids',
+        readonly=False,
+    )
