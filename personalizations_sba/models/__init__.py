27cc35eba3153b02501af86e1ec68f0772b55f77
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-12-02 16:50:29 -0300

    [FIX] personalizations

--- personalizations_sba/models/__init__.py
+++ personalizations_sba/models/__init__.py
@@ -16,6 +16,7 @@ from . import purchase_order_line
 from . import res_company
 from . import res_partner
 from . import sale_order_line
+from . import sale_order
 from . import stock_move
 from . import stock_picking
 from . import stock_quant
--- personalizations_sba/models/account_invoice.py
+++ personalizations_sba/models/account_invoice.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+from datetime import datetime
+import dateutil
 
 
 class AccountInvoice(models.Model):
@@ -13,25 +16,98 @@ class AccountInvoice(models.Model):
     def _compute_x_fecha_segundo_venc(self):
         for rec in self:
           if rec.date_due:
-            date = datetime.datetime.strptime(rec.date_due, "%Y-%m-%d") + dateutil.relativedelta.relativedelta(days=rec.company_id.x_rango_fecha_recargo)
-            rec['x_fecha_segundo_venc'] = datetime.datetime.strftime(date, "%Y-%m-%d")
+            date = datetime.strptime(rec.date_due, "%Y-%m-%d") + dateutil.relativedelta.relativedelta(days=rec.company_id.x_rango_fecha_recargo)
+            rec.x_fecha_segundo_venc = datetime.strftime(date, "%Y-%m-%d")
           else:
-            rec['x_fecha_segundo_venc'] = False
+            rec.x_fecha_segundo_venc = False
 
-    @api.depends('sale_order_ids','amount_total')
+    @api.depends('sale_order_ids', 'amount_total')
     def _compute_x_monto_cargo(self):
         for rec in self.filtered('sale_order_ids'):
-          rec['x_monto_cargo'] = rec.amount_total * (1 + (rec.sale_order_ids[0].pricelist_id.x_porcentaje_cargo_extra)/ 100)
+            rec.x_monto_cargo = rec.amount_total * (1 + (rec.sale_order_ids[0].pricelist_id.x_porcentaje_cargo_extra) / 100)
 
-    @api.depends('date_due','x_invoice_nd_id','state')
+    @api.depends('date_due', 'x_invoice_nd_id', 'state')
     def _compute_x_state_invoice(self):
         for rec in self:
           x_state_invoice = 'no_vencida'
           if rec.state != 'open':
-            rec['x_state_invoice'] = x_state_invoice
+            rec.x_state_invoice = x_state_invoice
             continue
-          if rec.date_due < datetime.datetime.today().strftime("%Y-%m-%d"):
+          if rec.date_due < fields.Date.today():
             x_state_invoice = 'vencida'
-          if x_state_invoice == 'vencida' and  not rec.x_invoice_nd_id:
+          if x_state_invoice == 'vencida' and not rec.x_invoice_nd_id:
             x_state_invoice = 'vencida_sin_nd'
-          rec['x_state_invoice'] = x_state_invoice
+          rec.x_state_invoice = x_state_invoice
+
+    @api.multi
+    def create_debt_invoice(self):
+        def prepare_interest_invoice(partner, amount, journal):
+            comment = "Recargo por mora de la factura {}".format(self.document_number)
+            account_id = self.account_id.id or partner.property_account_receivable_id.id
+
+            invoice_vals = {
+                'type': 'out_invoice',
+                'account_id': account_id,
+                'partner_id': partner.id,
+                'journal_id': journal.id,
+                'comment': comment,
+                'origin': self.document_number,
+                'currency_id': self.company_id.currency_id.id,
+                'payment_term_id': partner.property_payment_term_id.id or False,
+                'fiscal_position_id': partner.property_account_position_id.id,
+                'date_invoice': fields.Date.today(),
+                'company_id': self.company_id.id,
+                'user_id': partner.user_id.id or False
+            }
+            return invoice_vals
+
+        def prepare_interest_invoice_line(invoice, partner, amount):
+            company = self.company_id
+            try:
+              product = self.env['product.product'].browse(PRODUCT_ID)
+            except Exception:
+              raise UserError("No tienen configurado el producto")
+            line_values = {}
+            line_data = self.env['account.invoice.line'].with_context(
+                force_company=company.id).new(dict(
+                    product_id=PRODUCT_ID,
+                    quantity=1.0,
+                    invoice_id=invoice.id,
+                    partner_id=partner.id,
+                ))
+            line_data._onchange_product_id()
+
+            if not line_data.account_id:
+                raise UserError(
+                    'El producto {} no esta correctamente configurado, falta la cuenta contable.'.format(product.name))
+
+            line_data['price_unit'] = amount
+            line_data['name'] = line_data.product_id.name + '.\n' + invoice.comment
+
+
+            for field in line_data._cache:
+              line_values[field] = line_data[field]
+            values = line_data._convert_to_write(line_values)
+            return values
+
+        def create_invoice():
+            partner = self.partner_id
+            amount = self.x_monto_cargo - self.amount_total
+            journal = self.journal_id
+            invoice_vals = prepare_interest_invoice(partner, amount, journal)
+            invoice = self.with_context(internal_type='debit_note').create(invoice_vals)
+
+            self.env['account.invoice.line'].create(prepare_interest_invoice_line(invoice, partner, amount))
+            invoice.compute_taxes()
+            invoice.message_post(body="Factura de recargo por mora creada de la factura {}".format(self.document_number))
+            return invoice
+        self.ensure_one()
+        PRODUCT_ID = int(self.env['ir.config_parameter'].sudo().get_param('product.nd_mora'))
+        invoice = create_invoice()
+        self.x_invoice_nd_id = invoice
+        actions = self.env.ref('account.action_invoice_tree1')
+        action_read = actions.read()[0]
+        res = self.env.ref('account.invoice_form', False)
+        action_read['views'] = [(res and res.id or False, 'form')]
+        action_read['res_id'] = invoice.id
+        return action_read
