c0561f82d1087b222c3b4c09dd6eee6d1cb3c62c
nmadhoc <nm@adhoc.com.ar>
2020-12-02 17:36:20 -0300

    [MOD] Ticket 32994

---
+++ personalizations_gestionporcina/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Personalizations Gestión Porcina',
+    'version': '11.0.1.1.0',
+    'sequence': 14,
+    'summary': '',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'images': [
+    ],
+    'depends': [
+        'contacts'
+    ],
+    'data': [
+        'views/res_partner_views.xml'
+    ],
+    'application': False,
+    'installable': True,
+    'auto-install': False,
+}
