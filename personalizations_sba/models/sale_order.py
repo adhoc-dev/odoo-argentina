68b8b19616f53d795f8faa1af4191eb1d569a3e1
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-12-14 14:10:56 -0300

    [11.0] [FIX] personalizations_sba: Call super before.

--- personalizations_sba/models/sale_order.py
+++ personalizations_sba/models/sale_order.py
@@ -6,6 +6,7 @@ class SaleOrder(models.Model):
 
     @api.onchange('partner_id')
     def onchange_partner_id(self):
+        super(SaleOrder, self).onchange_partner_id()
         for rec in self.filtered(lambda x: not x.partner_id.sale_type):
             type_id = self.env['ir.default'].sudo().get('sale.order', 'type_id', company_id=rec.company_id.id, user_id=self.env.user.id)
             if type_id:
