c1a3be0fee51e3b00ec77183e6d1d53d8ed0786b
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-12-14 09:39:59 -0300

    [11.0] [FIX] personalizations_sba: Fix misspelling in the call of a method

--- personalizations_sba/models/sale_order.py
+++ personalizations_sba/models/sale_order.py
@@ -6,7 +6,7 @@ class SaleOrder(models.Model):
 
     @api.onchange('partner_id')
     def onchange_partner_id(self):
-        for rec in self.filered(lambda x: not x.partner_id.sale_type):
-            type_id = env['ir.default'].sudo().get('sale.order', 'type_id', company_id=rec.company_id.id, user_id=self.env.user.id)
+        for rec in self.filtered(lambda x: not x.partner_id.sale_type):
+            type_id = self.env['ir.default'].sudo().get('sale.order', 'type_id', company_id=rec.company_id.id, user_id=self.env.user.id)
             if type_id:
                 rec.type_id = type_id
