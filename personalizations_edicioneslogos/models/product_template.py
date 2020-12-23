ec48170e5aee1b9c1b05c3890a71232fa1b01974
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-12-23 13:37:12 -0300

    [11.0] [FIX] personalizations_edicioneslogos: Avoid problems when creating coupon programs.

--- personalizations_edicioneslogos/models/product_template.py
+++ personalizations_edicioneslogos/models/product_template.py
@@ -6,10 +6,10 @@ class ProductTemplate(models.Model):
 
     @api.constrains('name')
     def _check_product_uniq(self):
-        # Agregamos Discount en filtered ya que el modulo sale_coupon crea productos con el mismo nombre para los descuento
-        for rec in self.filtered(lambda x: x.name.find("discount") == -1):
-            nbr_product = self.env['product.template'].search_count([('name', '=', rec.name)])
-            if nbr_product > 1:
-                raise ValidationError(
-                    "No puede haber 2 productos con el mismo nombre, Eliga otro nombre")
+        if not self._context.get('default_program_type', False):
+            for rec in self:
+                nbr_product = self.env['product.template'].search_count([('name', '=', rec.name)])
+                if nbr_product > 1:
+                    raise ValidationError(
+                        "No puede haber 2 productos con el mismo nombre, Eliga otro nombre")
 
