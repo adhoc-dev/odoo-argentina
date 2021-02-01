a718d5ef1db2744d603598b837732fc54c03d1df
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-02-01 08:40:22 -0300

    [IMP] personalizations_arbusta: fix runbot warning

--- personalizations_arbusta/models/res_partner.py
+++ personalizations_arbusta/models/res_partner.py
@@ -9,5 +9,7 @@ class ResPartner(models.Model):
     x_zona = fields.Char(string="Zona")
     x_studio_field_qcof1 = fields.Char(string="Email personal", copy=False)
     x_studio_field_l5Gzo = fields.Date(string="Fecha de nacimiento", copy=False)
-    x_studio_field_j5Qb2 = fields.Integer(string="Edad", copy=False)
+    # agregamos espacio para evitar warning por label duplicado con otro
+    # modulo en este repo hasta que encontremos mejor solucion
+    x_studio_field_j5Qb2 = fields.Integer(string="Edad ", copy=False)
     x_studio_field_t95ga = fields.Boolean(string="Titular OS / Prepaga", copy=False)
