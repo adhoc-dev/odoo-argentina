a5d4b8dad47ebbcfab9307472515fa38cfbbfb16
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-02-12 11:15:32 -0300

    [FIX] neored: related fields as readonly False So that they can be written

--- neored_personalization/models/res_partner.py
+++ neored_personalization/models/res_partner.py
@@ -6,9 +6,9 @@ class ResPartner(models.Model):
 
     # for compatibility with odumbo till we migrate everything to v13
     # TODO remove on v14 (or before)
-    main_id_number = fields.Char(related='vat')
-    main_id_category_id = fields.Many2one(related='l10n_latam_identification_type_id')
-    afip_responsability_type_id = fields.Many2one(related='l10n_ar_afip_responsibility_type_id')
+    main_id_number = fields.Char(related='vat', readonly=False)
+    main_id_category_id = fields.Many2one(related='l10n_latam_identification_type_id', readonly=False)
+    afip_responsability_type_id = fields.Many2one(related='l10n_ar_afip_responsibility_type_id', readonly=False)
 
     def address_get(self, adr_pref=None):
         """ We modify this return for the case the contact has the label for claims when
