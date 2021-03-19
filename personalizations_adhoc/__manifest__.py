fa1f855dce0d70a8128e9d80dbc9340762b21701
nmadhoc <nm@adhoc.com.ar>
2021-03-19 10:24:20 -0300

    [ADD] personalizations_adhoc Creación del módulo de personalizaciones Contiene solo la re-escritura de la constrain _check_task_project 	Ahora chequea que el comercial del proyecto sea igual al comercial del proyecto de la tarea

---
+++ personalizations_adhoc/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'personalizations_adhoc',
+    'version': '13.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'depends': [
+        'hr_timesheet',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'OPL-1',
+}
