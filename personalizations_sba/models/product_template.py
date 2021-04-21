44befcc74111de613f57032706db9590cf053c99
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-04-21 09:58:11 -0300

    [FIX] personalizations_sba: add dummy space This is so that fields strings are unique (with ediciones logos personalizations module)

--- personalizations_sba/models/product_template.py
+++ personalizations_sba/models/product_template.py
@@ -5,8 +5,8 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     x_studio_field_5G9jj = fields.Char(string="ISBN", copy=False)
-    x_studio_field_EqNtz = fields.Many2one(string="Editorial", comodel_name="x_product_editorial", on_delete="set null", copy=False)
-    x_studio_field_euxHW = fields.Many2one(string="Autor", comodel_name="x_product_autor", on_delete="set null", copy=False)
+    x_studio_field_EqNtz = fields.Many2one(string="Editorial ", comodel_name="x_product_editorial", on_delete="set null", copy=False)
+    x_studio_field_euxHW = fields.Many2one(string="Autor ", comodel_name="x_product_autor", on_delete="set null", copy=False)
     x_studio_field_nqwdw = fields.Many2one(string="Versión", comodel_name="x_product_version", on_delete="set null", copy=False)
     x_studio_field_k203V = fields.Many2one(string="Idioma", comodel_name="x_product_language", on_delete="set null", copy=False)
     x_studio_field_NlwHP = fields.Many2one(string="Familia", comodel_name="x_product_familia", on_delete="set null", copy=False)
