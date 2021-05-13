7acad4c842c588d436c7054ceddaf48d2f5d34a7
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-05-13 13:06:22 +0000

    [MIG] personalizations_robertocala: Migration to 13.0
    X-original-commit: 71d3c512490144f2777a56fada528a5a170a9ca7
    Part-of: ingadhoc/personalizations#537

--- personalizations_robertocala/__manifest__.py
+++ personalizations_robertocala/__manifest__.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'personalizations_robertocala',
-    'version': '11.0.2.0',
+    'version': '13.0.1.0.0',
     'category': 'Personalizations',
     'author': 'ADHOC SA',
     'depends': [
