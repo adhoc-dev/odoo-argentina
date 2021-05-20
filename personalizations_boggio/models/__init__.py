052b9455e6fc0dff665b09b8cfbe179bdbf1caf0
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-05-20 15:40:44 -0300

    [FIX] stock_picking_batch adapt to new changes in v13

--- personalizations_boggio/models/__init__.py
+++ personalizations_boggio/models/__init__.py
@@ -26,7 +26,7 @@ from . import res_partner_industry
 from . import sale_order
 from . import sale_order_line
 from . import sale_order_type
-from . import stock_batch_picking
+from . import stock_picking_batch
 from . import stock_inventory
 from . import stock_location
 from . import stock_move
