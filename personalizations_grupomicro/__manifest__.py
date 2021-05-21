ae00722aa97c2655d0f21d9aecf8b410a52d5151
nmadhoc <nm@adhoc.com.ar>
2021-05-21 16:36:18 -0300

    [ADD] personalizations_grupomicro

---
+++ personalizations_grupomicro/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'personalizations_grupomicro',
+    'version': '13.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'depends': [
+
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'OPL-1',
+}
