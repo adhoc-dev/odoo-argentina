a0e5fe7462b188144987c01c227f47095b1bd5b0
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-06-04 10:18:36 -0300

    [IMP] Changes for partner debts in sale orders.

--- personalizations_boggio/models/sale_order.py
+++ personalizations_boggio/models/sale_order.py
@@ -6,9 +6,21 @@ class SaleOrder(models.Model):
 
     x_monto_USD = fields.Float(string="Monto Presupuesto en USD", help="Se graba el monto del presupuesto en dolares IVA incluido", compute='_compute_x_monto_USD', readonly=True, store=True, copy=False)
     x_info_cc = fields.Char(string="Info CC", readonly=True, copy=False)
+    total_overdue = fields.Monetary(related="partner_id.total_overdue")
 
     @api.depends('state', 'currency_id','amount_total')
     def _compute_x_monto_USD(self):
         for rec in self.filtered(lambda s: s.state == 'draft'):
             USD_currency = self.env.ref('base.USD')
             rec.x_monto_USD = rec.currency_id._convert(rec.amount_total, USD_currency, rec.company_id, fields.Date.today())
+
+    def open_action_followup(self):
+        self.ensure_one()
+        return {
+            'name': _("Overdue Payments for %s") % self.partner_id.display_name,
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'views': [[self.env.ref('account_followup.customer_statements_form_view').id, 'form']],
+            'res_model': 'res.partner',
+            'res_id': self.partner_id.id,
+        }
