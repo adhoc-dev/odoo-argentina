4b73696445bf3778511a654cd1f76acf5068a009
Pablo Paez <pablopaezps97@gmail.com>
2021-06-15 10:37:40 -0300

    [ADD]personalizatins_arbusta: origin and industry field in crm.lead

--- personalizations_arbusta/models/crm_lead.py
+++ personalizations_arbusta/models/crm_lead.py
@@ -16,7 +16,10 @@ class CrmLead(models.Model):
     x_studio_field_9ZMsr = fields.Selection(string="Origen",
                                             selection=[('Farming Cross-Selling', 'Farming Cross-Selling'),
                                                        ('Farming Upselling', 'Farming Upselling'),
-                                                       ('Hunting', 'Hunting')], copy=False)
+                                                       ('Hunting', 'Hunting'),('Hunting Networking', 'Hunting Networking'),
+                                                       ('Hunting Linkedin', 'Hunting Linkedin'),
+                                                       ('Hunting Agencia SQR', 'Hunting Agencia SQR'),
+                                                       ('Hunting Evento', 'Hunting Evento')], copy=False)
     x_studio_field_8wDdo = fields.Selection(string="Idioma requerido",
                                             selection=[('N/A', 'N/A'), ('Inglés oral', 'Inglés oral'),
                                                        ('Inglés escrito', 'Inglés escrito'),
@@ -35,3 +38,17 @@ class CrmLead(models.Model):
     x_studio_field_Wq8M7 = fields.Char(string="Link Propuesta", copy=False)
     x_studio_field_jyLyQ = fields.Boolean(string="Check Capacity", tracking=100, copy=False)
     x_project_currency = fields.Many2one(comodel_name='res.currency', string='Project Currency')
+    x_studio_field_QI5H4 = fields.Selection(string="Industria",
+                                        selection=[('Aeronáutica', 'Aeronáutica'),
+                                                    ('Agrobusiness', 'Agrobusiness'), ('Banca', 'Banca'),
+                                                    ('Beauty', 'Beauty'), ('Cryptomonedas', 'Cryptomonedas'),
+                                                    ('E-commerce & marketplace', 'E-commerce & marketplace'),
+                                                    ('Educación', 'Educación'),
+                                                    ('Electrónica', 'Electrónica'),
+                                                    ('Entretenimiento', 'Entretenimiento'),
+                                                    ('Fintech', 'Fintech'), ('Gobierno', 'Gobierno'),
+                                                    ('Marketplace', 'Marketplace'), ('Oil & Gas', 'Oil & Gas'),
+                                                    ('ONG', 'ONG'), ('Otra', 'Otra'), ('Paperless', 'Paperless'),
+                                                    ('Recursos Humanos', 'Recursos Humanos'), ('Salud', 'Salud'),
+                                                    ('Seguros', 'Seguros'), ('Tecnología', 'Tecnología'),
+                                                    ('N/A', 'N/A')], copy=False)
