1804548a84c7b12d20de1a0eeefaac2f7b0696f4
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-06-28 15:17:10 -0300

    [IMP] force to false the default of the company in supplierinfo

--- personalizations_boggio/models/product_supplierinfo.py
+++ personalizations_boggio/models/product_supplierinfo.py
@@ -5,3 +5,4 @@ class ProductSupplierinfo(models.Model):
     _inherit = 'product.supplierinfo'
 
     x_studio_field_5L26y = fields.Char(string="Codigo proveedor", related="replenishment_cost_rule_id.product_ids.supplier_code", help="This vendor's product code will be used when printing a request for quotation. Keep empty to use the internal one.", readonly=True, copy=False)
+    company_id = fields.Many2one(default=False)
