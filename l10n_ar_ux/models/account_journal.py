be25a725b52b54c224e5eb587d0c900a9da168e0
Katherine Zaoral <kz@adhoc.com.ar>
2021-09-28 22:07:57 -0300

    [ADD] l10n_ar_is_pos: liquido producto logic
    * add l10n_ar_is_pos field
    * remove not_applicable option from  afip pos system types
    * do not create sequences for use documents of type purchase

--- l10n_ar_ux/models/account_journal.py
+++ l10n_ar_ux/models/account_journal.py
@@ -18,13 +18,22 @@ class AccountJournal(models.Model):
         string="QR Code",
         help="String to generate the QR Code that will be displayed on the invoice report."
     )
+    l10n_ar_is_pos = fields.Boolean(compute="_compute_l10n_ar_is_pos", store=True, readonly=False, string="Is AFIP POS?")
 
-    def _get_l10n_ar_afip_pos_types_selection(self):
-        """ Add more options to the selection field AFIP POS System, re order options by common use """
-        res = super()._get_l10n_ar_afip_pos_types_selection()
-        res.append(('not_applicable', _('Not Applicable')),)
-        return res
+    @api.depends('l10n_latam_country_code', 'type', 'l10n_latam_use_documents')
+    def _compute_l10n_ar_is_pos(self):
+        ar_sale_use_documents = self.filtered(
+            lambda x: x.l10n_latam_country_code == 'AR' and x.type == 'sale' and x.l10n_latam_use_documents)
+        ar_sale_use_documents.l10n_ar_is_pos = True
+        (self - ar_sale_use_documents).l10n_ar_is_pos = False
 
-    @api.constrains('l10n_ar_afip_pos_number')
-    def _check_afip_pos_number(self):
-        return super(AccountJournal, self.filtered(lambda x: x.l10n_ar_afip_pos_system != 'not_applicable'))._check_afip_pos_number()
+    def _l10n_ar_create_document_sequences(self):
+        """ IF AFIP Configuration change try to review if this can be done and then create / update the document
+        sequences """
+        self.ensure_one()
+
+        if self.company_id.country_id == self.env.ref('base.ar') and self.l10n_latam_use_documents and self.type == 'purchase':
+            # TODO KZ improve, create purchase manually using document types
+            return False
+
+        return super()._l10n_ar_create_document_sequences()
