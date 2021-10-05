e7b8623283aacc91e7a49dd5d81071e18b6f3d28
Pablo Paez <pp@adhoc.com.ar>
2021-10-05 11:56:38 -0300

    [FIX]arbusta: others ortography errors

--- personalizations_arbusta/models/crm_lead.py
+++ personalizations_arbusta/models/crm_lead.py
@@ -7,8 +7,8 @@ class CrmLead(models.Model):
     x_studio_field_1IT3n = fields.Selection(string="Tipo de servicio",
                                             selection=[('Data Services', 'Data Services'),
                                                        ('Development', 'Development'),
-                                                       ('Costumer Experience',
-                                                        'Costumer Experience'),                                                       ('Machine Learning Training', 'Machine Learning Training'),
+                                                       ('Customer Experience',
+                                                        'Customer Experience'),                                                       ('Machine Learning Training', 'Machine Learning Training'),
                                                        ('QA & Testing', 'QA & Testing')], copy=False)
     x_studio_field_hkOoC = fields.Date(string="Inicio estimado", copy=False)
     x_studio_field_KPykh = fields.Integer(string="Horas mensuales", copy=False)
