702393b74256cbf9ba5b55a3b76985cbed402257
Joel Zilli <joz@adhoc.com.ar>
2021-12-06 19:10:43 +0000

    [IMP] Boggio: add reasons tags to returns
    closes ingadhoc/personalizations#386
    
    X-original-commit: ed91bda2f53f2d9e248d4f981e78a295de24dd7a
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- personalizations_boggio/models/x_devolucion_tags_devolucion_tags.py
+++ personalizations_boggio/models/x_devolucion_tags_devolucion_tags.py
@@ -6,3 +6,9 @@ class XDevolucionTagsDevolucionTags(models.Model):
     _description = 'x_devolucion_tags'
 
     x_name = fields.Char(string="Name", required=True)
+
+    def name_get(self):
+        result = []
+        for record in self:
+            result.append((record.id, record.x_name))
+        return result
