0664e8350cf1bdf7dd259b4453e2e1af81a262e1
Pablo Paez <pp@adhoc.com.ar>
2021-12-20 15:47:45 +0000

    [ADD]brunetti:analytic_tags required in pickings
    closes ingadhoc/personalizations#412
    
    X-original-commit: 8a00d998848b3e8129a51d879a08c4e88a1d68e4
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- personalizations_brunetti/models/__init__.py
+++ personalizations_brunetti/models/__init__.py
@@ -2,3 +2,4 @@ from . import account_move
 from . import crm_lead
 from . import crm_stage
 from . import sale_order
+from . import stock_picking
---
+++ personalizations_brunetti/models/stock_picking.py
@@ -0,0 +1,12 @@
+from odoo import models
+from odoo.exceptions import UserError
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def button_validate(self):
+        res = super().button_validate()
+        if self.move_ids_without_package.filtered(lambda x: not x.analytic_tag_ids):
+            raise UserError("Cada linea de la transferencia debe tener al menos una etiqueta analitica")
+        return res
