a3c6419cdb6b306f9e25e204a052f08cebe24908
nicolas <nmr@adhoc.com.ar>
2021-12-28 13:38:12 +0000

    Cherry pick of 39c7f3ff36910396926a8b02e26f6accbba08ffd failed
    stdout:
    Auto-merging personalizations_cotesma/models/stock_request_order.py
    CONFLICT (content): Merge conflict in personalizations_cotesma/models/stock_request_order.py
    
    stderr:
    15:20:48.572041 git.c:439               trace: built-in: git cherry-pick 39c7f3ff36910396926a8b02e26f6accbba08ffd
    error: could not apply 39c7f3f... [FIX] personalizations_cotesma: Add api depends.
    hint: after resolving the conflicts, mark the corrected paths
    hint: with 'git add <paths>' or 'git rm <paths>'
    hint: and commit the result with 'git commit'
    ----------
    status:
    
    closes ingadhoc/personalizations#413
    
    Signed-off-by: Pablo Paez Sheridan <pp@adhoc.com.ar>
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- personalizations_cotesma/models/stock_request_order.py
+++ personalizations_cotesma/models/stock_request_order.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class StockRequest(models.Model):
@@ -7,7 +8,17 @@ class StockRequest(models.Model):
     user_tag_allow_ids = fields.Many2many('product.template.tag', compute="_compute_tag_allow_ids")
 
     
+    @api.depends('warehouse_id')
+    @api.depends_context('uid')
     def _compute_tag_allow_ids(self):
         for rec in self:
             rec.user_tag_allow_ids = self.env.user.tag_ids
+
+    @api.model
+    def _create_from_product_multiselect(self, products):
+        if products:
+            product_without_access = products.filtered(lambda p: p.tag_ids not in self.env.user.tag_ids)
+            if product_without_access:
+                raise UserError('Estos productos: "%s" no tienen permisos para generar Pedido de existencias.' % ' - '.join(product_without_access.mapped('name')))
+        return super()._create_from_product_multiselect(products)
 
