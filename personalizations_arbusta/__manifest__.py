e8151084f3c239255442e52cf5c03821c9056c77
Pablo Paez <pp@adhoc.com.ar>
2021-12-30 13:26:35 +0000

    [FIX]arbusta:filter domain for anlytic line for employee
    closes ingadhoc/personalizations#410
    
    X-original-commit: 38f3be1acca8fb5b0151de449be1b96361bae3e0
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- personalizations_arbusta/__manifest__.py
+++ personalizations_arbusta/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'personalizations_arbusta',
-    'version': '13.0.1.26.0',
+    'version': '13.0.1.27.0',
     'category': 'Personalizations',
     'author': 'ADHOC SA',
     'depends': [
