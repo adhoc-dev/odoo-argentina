eb12a0cdaf3015ca7353d6613bc5d13aea279a20
Pablo Paez <pp@adhoc.com.ar>
2022-01-04 18:18:09 +0000

    [IMP]brunetti: analytic tags only required in incoming pickings
    closes ingadhoc/personalizations#415
    
    X-original-commit: df79e2eae774bd0f8e0062de10312c479c1223e6
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- personalizations_brunetti/models/stock_picking.py
+++ personalizations_brunetti/models/stock_picking.py
@@ -7,6 +7,6 @@ class StockPicking(models.Model):
 
     def button_validate(self):
         res = super().button_validate()
-        if self.move_ids_without_package.filtered(lambda x: not x.analytic_tag_ids):
+        if self.picking_type_code == "outgoing" and self.move_ids_without_package.filtered(lambda x: not x.analytic_tag_ids):
             raise UserError("Cada linea de la transferencia debe tener al menos una etiqueta analitica")
         return res
