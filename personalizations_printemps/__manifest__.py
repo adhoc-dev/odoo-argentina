76225ebb756cc64475886564702c539e2b16740a
Ignacio Cainelli <ica@adhoc.com.ar>
2022-05-12 17:36:25 +0000

    [ADD] printemps: create new compute field
    Se muestra un campo computado en la vista lista de los contactos,
    este campo indica la deuda de cada cliente
    
    closes ingadhoc/personalizations#546
    
    X-original-commit: 31ac34c6d6726ea16d762030f03d56f3f0b83c65
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

---
+++ personalizations_printemps/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Personalizations Printemps',
+    'version': '13.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/res_partner_view.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto-install': False,
+    'license': 'OPL-1',
+}
