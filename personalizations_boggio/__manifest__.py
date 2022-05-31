fba5843ae6cfad0695fb0a9388a15297f4346e41
Virginia Bonservizi <vib@adhoc.com.ar>
2022-05-31 14:25:18 +0000

    [ADD]personalizations_boggio: cambios sobre las vistas de devoluciones
    closes ingadhoc/personalizations#596
    
    X-original-commit: 040a5d5f554e4c31b56c8fee3e01b71f0f679859
    Signed-off-by: Joel Zilli <joz@adhoc.com.ar>
    Signed-off-by: Virginia Bonservizi <vib@adhoc.com.ar>

--- personalizations_boggio/__manifest__.py
+++ personalizations_boggio/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'personalizations_boggio',
-    'version': '13.0.1.15.0',
+    'version': '13.0.1.16.0',
     'category': 'Personalizations',
     'author': 'ADHOC SA',
     'depends': [
