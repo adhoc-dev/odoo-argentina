60f72a20e035c8ba049c94a518473dd201883512
nicolas <nmr@adhoc.com.ar>
2022-06-28 18:37:03 +0000

    [FIX] winwin: fix name of the new models created by interfase.
    closes ingadhoc/personalizations#615
    
    X-original-commit: e6e18b11c4c07ef56a00235b03ff9c8439e4b420
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- personalizations_winwineducation/models/x_motive_rejected_proposal.py
+++ personalizations_winwineducation/models/x_motive_rejected_proposal.py
@@ -4,5 +4,6 @@ from odoo import models, fields
 class XMotiveRejectedProposal(models.Model):
     _name = 'x_motive_rejected_proposal'
     _description = 'Motivos Rechazo Propuesta Laboral'
+    _rec_name = 'x_name'
 
     x_name = fields.Char(string="Motivo", required=True, copy=False)
--- personalizations_winwineducation/models/x_res_partner_emergency_contact.py
+++ personalizations_winwineducation/models/x_res_partner_emergency_contact.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class XResPartnerEmergencyContact(models.Model):
     _name = 'x_res.partner.emergency.contact'
     _description = 'Contacto de emergencia'
+    _rec_name = 'x_name'
 
     x_name = fields.Char(string="Name", copy=False)
     x_type = fields.Selection(string="Tipo", selection=[('a','Autorización'),('r','Restricción'),('ce','Contacto de Emergencia'),('ie','Institución de Emergencia')])
