89e08c959c517cbe2fee908ce61b55632c89ba13
Augusto Weiss <awe@adhoc.com.ar>
2022-07-26 00:25:41 +0000

    [FIX] personalizations_boggio: remove field deprecated by migration
    closes ingadhoc/personalizations#652
    
    X-original-commit: e85e8f6852cae18dcb37987b4299e9f0be2d84d8
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>
    Signed-off-by: Augusto Weiss <awe@adhoc.com.ar>

--- personalizations_boggio/__manifest__.py
+++ personalizations_boggio/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'personalizations_boggio',
-    'version': '13.0.1.13.0',
+    'version': '13.0.1.14.0',
     'category': 'Personalizations',
     'author': 'ADHOC SA',
     'depends': [
