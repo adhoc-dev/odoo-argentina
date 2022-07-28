a5aaba5a8e5a558219c49afd935e64864e0e8605
Virginia Bonservizi <vib@adhoc.com.ar>
2022-07-28 14:19:49 +0000

    [ADD]personalizations_netnix: new project group
    closes ingadhoc/personalizations#673
    
    X-original-commit: 483e6a74958f5411a9966447fcc6952c87741467
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>
    Signed-off-by: Virginia Bonservizi <vib@adhoc.com.ar>

---
+++ personalizations_netnix/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Personalizations Netnix',
+    'version': '13.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'security/netnix_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': False,
+    'application': False,
+    'auto-install': False,
+    'license': 'OPL-1',
+}
