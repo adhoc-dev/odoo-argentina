a5520fab3cdf7ab552668d84a9801402de34ea3f
Ignacio Cainelli <ica@adhoc.com.ar>
2022-08-04 19:04:10 +0000

    [ADD] sgfintech: show column in account_move_line
    Se muestra el campo tax_base_amount en la vista tree de
    account_move_line
    
    closes ingadhoc/personalizations#736
    
    X-original-commit: cda5c5f7f19bd771503f8b67289d35aa17375031
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

---
+++ personalizations_sgfintech/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Personalizations SGfintech',
+    'version': '13.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_move_line_views.xml'
+    ],
+    'installable': True,
+}
