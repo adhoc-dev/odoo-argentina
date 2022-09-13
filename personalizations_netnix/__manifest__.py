1ab64ede6d41372b68c3f45cb8b33bf9f8c6632f
Virginia Bonservizi <vib@adhoc.com.ar>
2022-09-13 17:42:28 +0000

    [MIG] personalizations_netnix: Migration to 15.0
    closes ingadhoc/personalizations#752
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- personalizations_netnix/__manifest__.py
+++ personalizations_netnix/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Personalizations Netnix',
-    'version': '13.0.1.2.0',
+    'version': "15.0.1.0.0",
     'category': 'Personalizations',
     'author': 'ADHOC SA',
     'depends': [
@@ -13,7 +13,7 @@
         'security/ir.model.access.csv',
         'data/cron.xml'
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
     'auto-install': False,
     'license': 'OPL-1',
