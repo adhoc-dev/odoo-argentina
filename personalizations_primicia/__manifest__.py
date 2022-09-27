e8dcc68c6077e9678d1c76eea98c116851061ce3
juancarreras97 <jc@adhoc.com.ar>
2022-09-27 14:10:53 +0000

    [MIG] personalizations_primicia: Migration to 15.0
    closes ingadhoc/personalizations#779
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- personalizations_primicia/__manifest__.py
+++ personalizations_primicia/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Personalizations Primicia',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'category': 'Personalizations',
     'author': 'ADHOC SA',
     'depends': [
@@ -10,7 +10,7 @@
         'report/stock_report_views.xml',
         'report/report_package_barcode.xml',
     ],
-    'installable': False,
+    'installable': True,
     'application': False,
     'auto-install': False,
     'license': 'OPL-1',
