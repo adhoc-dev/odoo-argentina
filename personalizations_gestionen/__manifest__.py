636c7e20ac98dceb3e68c3c97ae64a89865834da
Ignacio Cainelli <ica@adhoc.com.ar>
2022-09-28 15:45:41 +0000

    [ADD] personalizations_gestionen: init module
    Primera personalizacion, correspondiente al ticket 53892
    
    Impresion de Recibos comunes
    
    Se crea un reporte para account_group_payment heredando el reporte
    de l10n_ar_ux, se eliminan campos que no se requieren, se cambia
    el nombre de reporte por 'Recibo' y se agrega una leyenda 'Recibí
    conforme'
    
    closes ingadhoc/personalizations#781
    
    X-original-commit: 1a505163c3757172cc8e6aeebcb96e81d79bf522
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>
    Signed-off-by: Nacho Cainelli <ica@adhoc.com.ar>

---
+++ personalizations_gestionen/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "personalizations_gestionen",
+    'version': "15.0.1.0.0",
+    'author': "Adhoc SA",
+    'category': 'Personalizations',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_ar_ux',
+    ],
+    'data': [
+        'reports/report_payment_group.xml',
+    ],
+    'demo': [
+    ],
+    'installable': False,
+    'auto_install': False,
+    'application': False,
+}
