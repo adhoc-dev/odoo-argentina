edb88f33a0476b4d72534f2e71eac0f4f08dce45
Virginia Bonservizi <vib@adhoc.com.ar>
2022-10-20 15:25:52 +0000

    [ADD] p13n_deltasmart: report modifications
    closes ingadhoc/personalizations#844
    
    X-original-commit: f7ea4e84cddb04161412ce9e9d40a1eb86498cb7
    Signed-off-by: Augusto Weiss <awe@adhoc.com.ar>
    Signed-off-by: Virginia Bonservizi <vib@adhoc.com.ar>

---
+++ p13n_deltasmart/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Personalizations Delta Smart',
+    'version': '13.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'depends': [
+        'l10n_ar_sale',
+        'l10n_ar_stock',
+    ],
+    'data': [
+        'views/report_deliveryslip.xml',
+        'views/sale_report_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto-install': False,
+    'license': 'OPL-1',
+}
