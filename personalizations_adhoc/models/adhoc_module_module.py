d4a6a3e9e243da18198d36f68ce82712e3c76027
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-10-27 09:10:49 +0000

    [IMP] _adhoc: adhoc product stored on module version
    closes ingadhoc/personalizations#830
    
    Related: ingadhoc/odoo-saas#180
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- personalizations_adhoc/models/adhoc_module_module.py
+++ personalizations_adhoc/models/adhoc_module_module.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class AdhocModuleModule(models.Model):
     _inherit = 'adhoc.module.module'
 
-    adhoc_product_id = fields.Many2one(related='module_id.adhoc_product_id')
+    adhoc_product_id = fields.Many2one(related='module_id.adhoc_product_id', store=True)
