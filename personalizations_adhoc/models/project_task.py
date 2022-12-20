ffede2085e7ac3a64ed1e6d6d419eecc80914be4
Bruno Zanotti <bz@adhoc.com.ar>
2022-12-20 11:57:28 -0300

    [IMP] adhoc: add ID on task's display name

--- personalizations_adhoc/models/project_task.py
+++ personalizations_adhoc/models/project_task.py
@@ -9,6 +9,12 @@ class ProjectTask(models.Model):
         domain=[('parent_id', '!=', False)])
     ticket_ids = fields.One2many('helpdesk.ticket', 'task_id')
 
+    def name_get(self):
+        result = []
+        for task in self:
+            result.append((task.id, "%s (#%d)" % (task.name, task._origin.id)))
+        return result
+
     @api.depends('adhoc_module_id.adhoc_product_id')
     def _compute_adhoc_product(self):
         for rec in self.filtered('adhoc_module_id.adhoc_product_id'):
