dd488a1a03115206bf7f99c17c6623379b679a27
José Antonio Keh <jok@adhoc.com.ar>
2022-12-28 12:05:07 +0000

    [IMP] p13n_vatten: FSM personalization
    Part-of: ingadhoc/personalizations#881

--- p13n_vatten/models/project.py
+++ p13n_vatten/models/project.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class ProjectTask(models.Model):
@@ -10,6 +10,18 @@ class ProjectTask(models.Model):
                                         tracking=True, readonly=False)
     instrucciones = fields.Html(string='Instrucciones', readonly=False)
     adjuntos = fields.Many2many('ir.attachment', string="Adjuntos", readonly=False)
+
+    order_type = fields.Selection(selection=[('control_de_aguas', 'Control Analítico de Aguas'),
+                                             #                                             ('control_de_efluentes', 'Control Analítico de Efluentes'),
+                                             #                                             ('control_de_corrosion', 'Medición de Tasa de Corrosión'),
+                                             ('informe_tecnico', 'Informe Técnico')],
+                                  default='control_de_aguas',
+                                  required=True,
+                                  tracking=True,
+                                  string='Tipo de Orden',
+                                  help='Ingrese el tipo de control a realizar')
+    recomendaciones = fields.Html(string='Recomendaciones', tracking=True,
+                                  readonly=False)
 
     @api.onchange('determinacion_ids')
     def _onchange_determinacion_ids(self):
@@ -58,3 +70,10 @@ class ProjectTask(models.Model):
                     'in_chart': parametro.in_chart,
                     }))
         self.update({'determinacion_ids': r })
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('project.task') or _('New')
+        result = super(ProjectTask, self).create(vals)
+        return result
