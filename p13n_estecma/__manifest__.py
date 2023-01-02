ece0303e8f08eb88c5855b405c51a26085dfabb7
Virginia Bonservizi <vib@adhoc.com.ar>
2023-01-02 14:42:13 +0000

    [ADD] p13n_estecma: create module and add group to salesperson
    closes ingadhoc/personalizations#912
    
    Signed-off-by: Augusto Weiss <awe@adhoc.com.ar>

---
+++ p13n_estecma/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Personalizations Estecma',
+    'version': '15.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'security/estecma_security.xml',
+        'views/account_move_views.xml',
+        'views/res_partner_views.xml',
+        'views/sale_order_views.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'license': 'OPL-1',
+}
