cc7bcf8c03ba99361967c5de43fb6524b3f9e9a8
Virginia Bonservizi <vib@adhoc.com.ar>
2023-02-13 20:20:41 +0000

    [FIX] personalizations_adhoc: manifest version
    closes ingadhoc/personalizations#945
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- personalizations_adhoc/__manifest__.py
+++ personalizations_adhoc/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'personalizations_adhoc',
-    'version': "15.0.1.13.0",
+    'version': "15.0.1.14.0",
     'category': 'Personalizations',
     'author': 'ADHOC SA',
     'depends': [
