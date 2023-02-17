929cf2c5a2edbb3b7ff3085d73fbed452c6c476c
Virginia Bonservizi <vib@adhoc.com.ar>
2023-02-17 17:15:38 +0000

    [ADD] p13n_integra: edit portal template
    closes ingadhoc/personalizations#950
    
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

--- p13n_integra/__manifest__.py
+++ p13n_integra/__manifest__.py
@@ -1,15 +1,20 @@
 {
     'name': 'Personalizations Integra',
-    'version': "15.0.1.0.0",
+    'version': "15.0.1.1.0",
     'category': 'Personalizations',
     'author': 'ADHOC SA',
     'depends': [
         'account',
+        'hr_timesheet',
         'payment',
+        'project',
+        'purchase',
+        'sale',
     ],
     'data': [
         'data/ir_actions_server.xml',
         'data/mail_template_data.xml',
+        'views/portal_templates.xml',
     ],
     'installable': True,
     'application': False,
