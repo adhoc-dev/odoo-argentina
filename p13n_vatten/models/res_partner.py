efc6e083f449e2ff1e2f22037d90d4ff3c9f5c24
José Antonio Keh <jok@adhoc.com.ar>
2023-02-27 13:26:46 +0000

    [IMP]p13n_vatten: changes contact type
    Part-of: ingadhoc/personalizations#881

--- p13n_vatten/models/res_partner.py
+++ p13n_vatten/models/res_partner.py
@@ -8,5 +8,4 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    type = fields.Selection(selection_add=[('service', 'Dirección de servicio')])
     muestra_ids = fields.One2many(comodel_name='muestras', inverse_name='partner_service_id', string="Puntos de Muestreo")
