5a0f67dd1be895b2b08ca5ab354c0b7e47eacd58
Virginia Bonservizi <vib@adhoc.com.ar>
2023-03-27 12:23:58 +0000

    [ADD] p13n_sf500: create module and show industry field for all contacts
    closes ingadhoc/personalizations#972
    
    Signed-off-by: augusto-weiss <awe@adhoc.com.ar>

---
+++ p13n_sf500/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Personalizations sf500',
+    'version': '15.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'license': 'OPL-1',
+}
