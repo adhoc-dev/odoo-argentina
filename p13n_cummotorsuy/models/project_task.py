e08d0600663c61376ecbc3491e825fd3ce09360b
mav-adhoc <mav@adhoc.com.ar>
2023-06-28 14:48:10 +0000

    [ADD] p13n_cummotorsuy: add fields on project task
    closes ingadhoc/personalizations#1073
    
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>

---
+++ p13n_cummotorsuy/models/project_task.py
@@ -0,0 +1,12 @@
+from odoo import models, fields
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    odometro = fields.Integer(readonly=False)
+    cms_uom = fields.Selection(
+        [('km','Km'),('horas','Horas'),], readonly=False)
+    lugar_de_reparacion = fields.Selection(
+        [('taller','Taller'),('campo','Campo'),], readonly=False)
+    distancia_a_campo = fields.Integer(readonly=False)
