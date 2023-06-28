818d77a3adc953cbd0af1456db4e7976e6918609
mav-adhoc <mav@adhoc.com.ar>
2023-06-28 11:48:10 -0300

    [ADD] p13n_cummotorsuy: add fields on project task

---
+++ p13n_cummotorsuy/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Personalizations Cummotorsuy',
+    'version': '15.0.1.0.0',
+    'category': 'Personalizations',
+    'author': 'ADHOC SA',
+    'license': 'AGPL-3',
+    'depends': [
+        'contacts',
+        'project',
+    ],
+    'data': [
+        'views/project_task_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
